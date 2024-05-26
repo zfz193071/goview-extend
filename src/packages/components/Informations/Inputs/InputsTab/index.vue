666ed2e582af3178ad9f37298db4ac67f339f359
zfz193071 <zfz193071@outlook.com>
2024-05-26 12:42:21 +0800

    ✨ feat(标签选择器): 暴露标签选择器的标签属性值列表

--- src/packages/components/Informations/Inputs/InputsTab/index.vue
+++ src/packages/components/Informations/Inputs/InputsTab/index.vue
@@ -9,9 +9,9 @@ import { PropType, toRefs, shallowReactive, watch } from 'vue'
 import cloneDeep from 'lodash/cloneDeep'
 import { CreateComponentType } from '@/packages/index.d'
 import { useChartEditStore } from '@/store/modules/chartEditStore/chartEditStore'
-import { useChartInteract, useExposedMethod } from '@/hooks'
+import { useChartInteract, useExposedProp } from '@/hooks'
 import { InteractEventOn } from '@/enums/eventEnum'
-import type { ExposedMethodType } from '@/packages/index.d'
+import type { ExposedPropType } from '@/packages/index.d'
 import { ComponentInteractParamsEnum } from './interact'
 
 const props = defineProps({
@@ -20,21 +20,21 @@ const props = defineProps({
     required: true
   }
 })
-const test = (value: any) => console.info('test : ==================>', JSON.stringify(value))
-const componentExposedMethods: Array<ExposedMethodType> = [
-  {
-    value: 'test',
-    label: '测试',
-    handler: test
-  }
-]
+
 const { w, h } = toRefs(props.chartConfig.attr)
 const option = shallowReactive({
   value: cloneDeep(props.chartConfig.option)
 })
+const componentExposedProps: Array<ExposedPropType> = option.value.dataset.map(a => {
+  return {
+    ...a,
+    defaultValue: a.value,
+    isolate: 1
+  }
+})
 defineExpose({
-  // 组件对外暴露的方法
-  ...useExposedMethod(props.chartConfig, componentExposedMethods)
+  // 组件对外暴露的状态
+  ...useExposedProp(props.chartConfig, componentExposedProps),
 })
 // 监听事件改变
 const onChange = (v: string) => {
